409f9a7cad0c3ed6dda4a3435fd6d515efa77cc7
furkantok <furkan.tok@useinsider.com>
2024-02-04 21:56:45 +0300

    last commit

--- tests/test_check_add_to_cart.py
+++ tests/test_check_add_to_cart.py
@@ -12,20 +12,14 @@ class TestCheckAddToCart(BaseTest):
         landing_page = LandingPage(self.driver)
         landing_page.is_landing_successful()
         landing_page.enter_username(self.username)
-        time.sleep(2)
         landing_page.enter_password(self.password)
-        time.sleep(2)
 
         inventory_page = landing_page.click_login_button()
-        time.sleep(2)
 
         product_page = inventory_page.click_product()
-        time.sleep(2)
         product_page.click_add_to_cart_button()
-        time.sleep(2)
 
         cart_page = product_page.click_cart_button()
-        time.sleep(2)
         cart_page.verify_product_is_added()
 
 
